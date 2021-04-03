c8ea3194062d970499b63056b657bbcdcbbca51b
hemangi-coder <81115378+hemangi-coder@users.noreply.github.com>
2021-04-03 11:07:02 +0530

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
 from speedometer import *
-
+import nltk
+nltk.download('vader_lexicon')
 
 def sentiment_analysis(message):
     sia = SIA()
